bcd2e48fef65f581efa447e16940908d76107ad3
ShimantaBhuyan <bhuyanshimanta@gmail.com>
2023-02-24 17:55:54 +0530

    feat: update hero image format to webp

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import { LogoSVG } from "./LogoSVG";
 
 export const Loader = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-[url('/assets/bg.png')] h-[100vh] w-[100vw] bg-cover bg-bottom ">
+    <div className="flex flex-col justify-center items-center bg-[url('/assets/bg.webp')] h-[100vh] w-[100vw] bg-cover bg-bottom sm:bg-[right_10px_bottom]">
       <LogoSVG color="black" />
       <h1 className="text-4xl sm:text-7xl sm:w-[75%] text-center font-medium bg-clip-text text-transparent bg-gradient-to-b from-gray-900 via-purple-900 to-violet-600">
         welcome to my space in the <br />
